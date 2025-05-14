bd6cdfc1b3577ffb81709e347b224e5eed68c724
Md Munna Mia <mdmunnamia987@gmail.com>
2025-05-14 16:53:51 +0600

    add basic hashing example

--- PH05_Hashing/counter.go
+++ PH05_Hashing/counter.go
@@ -1,1 +1,22 @@
 package hashing
+
+// Problem statement:
+// Given an array of integers: [1, 2, 1, 3, 2] 
+// and we are given some queries: [1, 3, 4, 2, 10]. 
+// For each query, we need to find out how many times the number 
+// appears in the array. For example, if the query is 1 our answer 
+// would be 2, and if the query is 4 the answer will be 0. 
+func Counter(arr, query []int) map[int]int {
+	result := make(map[int]int)
+
+	// initialize the key's from query array and assign the with 0
+	for i:=range query {
+		result[query[i]]=0
+	}
+
+	// counting there repeations
+	for i:=range arr {
+		result[arr[i]]++
+	}
+	return result
+}
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	recursion "github.com/munnaMia/DSA/Recursion"
+	hashing "github.com/munnaMia/DSA/PH05_Hashing"
 )
 
 func main() {
-	fmt.Println(recursion.Palidrome("abcsdba", 0, 4))
+	fmt.Println(hashing.Counter([]int{3, 2, 3, 4, 2, 1,43, 1, 1, 1}, []int{3, 2, 1, 4, 5}))
 }
